391a351915ea43b05ea6b59272b50545da30868b
Garrett HE <garrett.he@outlook.com>
2023-09-15 12:53:35 +0800

    Export global API functions

---
+++ etc/api.lua
@@ -0,0 +1,7 @@
+local _, FlexCommand = ...
+
+-- FlexCommand.command module
+FC_RegisterCommand = FlexCommand.command.RegisterCommand
+FC_UnregisterCommand = FlexCommand.command.UnregisterCommand
+FC_GetAllRegisteredCommands = FlexCommand.command.GetAllRegisteredCommands
+FC_ExecuteCommand = FlexCommand.command.ExecuteCommand
