18e623dc2a0df8a983195944edaca05f161e4c66
Garrett HE <garrett.he@outlook.com>
2023-09-21 10:37:53 +0800

    Export API functions

---
+++ etc/api.lua
@@ -0,0 +1,29 @@
+local _, FlexCommand = ...
+
+-- FlexCommand.config module
+FC_GetConfig = FlexCommand.config.GetConfig
+FC_SetConfig = FlexCommand.config.SetConfig
+
+-- FlexCommand.command module
+FC_RegisterCommand = FlexCommand.command.RegisterCommand
+FC_UnregisterCommand = FlexCommand.command.UnregisterCommand
+FC_GetAllRegisteredCommands = FlexCommand.command.GetAllRegisteredCommands
+FC_ExecuteCommand = FlexCommand.command.ExecuteCommand
+
+-- FlexCommand.event module
+FC_RegisterEvent = FlexCommand.event.RegisterEvent
+FC_RaiseEvent = FlexCommand.event.RaiseEvent
+
+-- FlexCommand.logging module
+FC_CreateLogger = FlexCommand.logging.CreateLogger
+FC_GetDefaultLogger = FlexCommand.logging.GetDefaultLogger
+FC_SetDefaultLogger = FlexCommand.logging.SetDefaultLogger
+FC_PrintError = FlexCommand.logging.PrintError
+FC_PrintWarning = FlexCommand.logging.PrintWarning
+FC_PrintInfo = FlexCommand.logging.PrintInfo
+FC_PrintDebug = FlexCommand.logging.PrintDebug
+
+-- FlexCommand.profile module
+FC_RegisterProfile = FlexCommand.profile.RegisterProfile
+FC_UnregisterProfile = FlexCommand.profile.UnregisterProfile
+FC_GetAllRegisteredProfiles = FlexCommand.profile.GetAllRegisteredProfiles
