db4af6926ed5ffad2684d7de28c44517f234380e
Garrett HE <garrett.he@outlook.com>
2023-09-21 11:28:08 +0800

    Add default commands

---
+++ etc/commands.lua
@@ -0,0 +1,23 @@
+FC_RegisterCommand("help", "Show registered commands", function()
+    print(string.format("FlexCommand version: %s", GetAddOnMetadata("FlexCommand", "Version")))
+
+    for command, spec in pairs(FC_GetAllRegisteredCommands()) do
+        print(string.format("|cffffff00%s|r: %s", command, spec["help"]))
+    end
+end)
+
+FC_RegisterCommand("config-get", "Get configuration value by key", function(args)
+    local key = args["key"]
+    local value = FC_GetConfig(key)
+
+    if value == nil then
+        FC_PrintError("Configuration with key '%s' not assigned yet.", key)
+        return
+    end
+
+    FC_PrintInfo("Configuration: %s = %s", key, value)
+end)
+
+FC_RegisterCommand("config-set", "Set configuration value by key", function(args)
+    FC_SetConfig(args["key"], args["value"])
+end)
