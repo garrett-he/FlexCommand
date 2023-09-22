eb0e3f302234b861f1375cdc8af0997f16e55f8f
Garrett HE <garrett.he@outlook.com>
2023-09-22 11:16:45 +0800

    Export logging functions

--- etc/api.lua
+++ etc/api.lua
@@ -9,3 +9,12 @@ FC_RegisterCommand = FlexCommand.command.RegisterCommand
 FC_UnregisterCommand = FlexCommand.command.UnregisterCommand
 FC_GetAllRegisteredCommands = FlexCommand.command.GetAllRegisteredCommands
 FC_ExecuteString = FlexCommand.command.ExecuteString
+
+-- FlexCommand.logging module
+FC_CreateLogger = FlexCommand.logging.CreateLogger
+FC_GetDefaultLogger = FlexCommand.logging.GetDefaultLogger
+FC_SetDefaultLogger = FlexCommand.logging.SetDefaultLogger
+FC_PrintError = FlexCommand.logging.PrintError
+FC_PrintWarning = FlexCommand.logging.PrintWarning
+FC_PrintInfo = FlexCommand.logging.PrintInfo
+FC_PrintDebug = FlexCommand.logging.PrintDebug
