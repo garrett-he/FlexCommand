7958ef13a1106218f08f97c845e8fc5b14307382
Garrett HE <garrett.he@outlook.com>
2023-09-23 12:12:44 +0800

    Add commands for profiles

--- etc/commands.lua
+++ etc/commands.lua
@@ -5,3 +5,13 @@ FC_RegisterCommand("help", "Show registered commands", function()
         print(string.format("|cffffff00%s|r: %s", command, spec["help"]))
     end
 end)
+
+FC_RegisterCommand("profile-list", "List all registered profiles", function()
+    for name, profile in pairs(FC_GetAllRegisteredProfiles()) do
+        print(string.format("%s: %s", name, profile.__loaded and "|cff00ff00Enabled|r" or "|cffff0000Disabled|r"))
+    end
+end)
+
+FC_RegisterCommand("profile-load", "Load the specified profile", function(args)
+    FC_LoadProfile(args["name"])
+end)
